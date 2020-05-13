f1db9d512debd9892013fe020a41717cd5f4a7b4
yomarsanchez <yomarsan.98@gmail.com>
2020-05-13 02:42:01 -0500

    Dark mode working and refactoring css variables.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# social-media-dashboard
-Social media dashboard with theme switcher
+<p align="left">
+  <a href="https://travis-ci.org/laravel/framework">
+    <img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status">
+  </a>
+  <a href="https://packagist.org/packages/laravel/framework">
+    <img src="https://poser.pugx.org/laravel/framework/license.svg" alt="License">
+  </a>
+</p>
+
+## Social media dashboard with theme switcher
+
+Proyecto desarrollado gracias al **#LiveLiveCoding #1** en el canal de [**@LeonidasEsteban**](https://www.youtube.com/leonidasesteban).<br>
+Demo https://yomarsanchez.github.io/social-media-dashboard
+
+- **Video Youtube :** [Responsive Design Mobile first & Dark Mode | Frontend Mentor challenge #1](https://youtu.be/ScSHSI7TzRw).
+
+### Resultado esperado
+
+<a href="./images/dark-mode.png">
+  <img src="./images/dark-mode.png" width="100%">
+</a>
+
+### License
+
+The project is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
--- js/app.js
+++ js/app.js
@@ -1,5 +1,37 @@
 const checkbox = document.querySelector('#checkbox')
+const themes = {
+  lightTheme: {
+    '--bgToggle': 'hsl(230, 22%, 74%)',
+    '--bgTheme': 'hsl(0, 0%, 100%)',
+    '--bgTopPatternTheme': 'hsl(225, 100%, 98%)',
+    '--bgCardTheme': 'hsl(227, 47%, 96%)',
+    '--textPrimary': 'hsl(230, 17%, 14%)',
+    '--textSecondary': 'hsl(228, 12%, 44%)',
+  },
+  darkTheme: {
+    '--bgToggle':
+      'linear-gradient(to right, hsl(210, 78%, 56%) 0%, hsl(146, 68%, 55%) 100%)',
+    '--bgTheme': 'hsl(230, 17%, 14%)',
+    '--bgTopPatternTheme': 'hsl(232, 19%, 15%)',
+    '--bgCardTheme': 'hsl(228, 28%, 20%)',
+    '--textPrimary': 'hsl(0, 0%, 100%)',
+    '--textSecondary': 'hsl(228, 34%, 66%)',
+  },
+}
 
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
   checkbox.setAttribute('checked', true)
+}
+
+checkbox.addEventListener('change', (event) => {
+  event.target.checked
+    ? setThemeProperties(themes.darkTheme)
+    : setThemeProperties(themes.lightTheme)
+})
+
+function setThemeProperties(props = {}) {
+  for (const prop in props) {
+    if (props.hasOwnProperty(prop))
+      document.body.style.setProperty(prop, props[prop])
+  }
 }
