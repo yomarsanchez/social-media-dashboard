5d33eda1d927380487ec8093262f96c0fd1a62a3
yomarsanchez <yomarsan.98@gmail.com>
2020-05-16 15:29:07 -0500

    Added card footer is danger.

--- js/app.js
+++ js/app.js
@@ -2,18 +2,18 @@ const checkbox = document.querySelector('#checkbox')
 const themes = {
   lightTheme: {
     '--bgToggle': 'hsl(230, 22%, 74%)',
-    '--bgTheme': 'hsl(0, 0%, 100%)',
-    '--bgTopPatternTheme': 'hsl(225, 100%, 98%)',
-    '--bgCardTheme': 'hsl(227, 47%, 96%)',
+    '--bgColor': 'hsl(0, 0%, 100%)',
+    '--topPatternBg': 'hsl(225, 100%, 98%)',
+    '--cardBg': 'hsl(227, 47%, 96%)',
     '--textPrimary': 'hsl(230, 17%, 14%)',
     '--textSecondary': 'hsl(228, 12%, 44%)',
   },
   darkTheme: {
     '--bgToggle':
       'linear-gradient(to right, hsl(210, 78%, 56%) 0%, hsl(146, 68%, 55%) 100%)',
-    '--bgTheme': 'hsl(230, 17%, 14%)',
-    '--bgTopPatternTheme': 'hsl(232, 19%, 15%)',
-    '--bgCardTheme': 'hsl(228, 28%, 20%)',
+    '--bgColor': 'hsl(230, 17%, 14%)',
+    '--topPatternBg': 'hsl(232, 19%, 15%)',
+    '--cardBg': 'hsl(228, 28%, 20%)',
     '--textPrimary': 'hsl(0, 0%, 100%)',
     '--textSecondary': 'hsl(228, 34%, 66%)',
   },
